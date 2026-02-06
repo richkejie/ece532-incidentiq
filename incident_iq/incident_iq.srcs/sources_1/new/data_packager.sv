98790c4068309eb7094e79179ff55eccfe5e3f60
RichardPC <richkejie@gmail.com>
2026-02-06 17:21:48 -0500

    added data_packager module

---
+++ incident_iq/incident_iq.srcs/sources_1/new/data_packager.sv
@@ -0,0 +1,53 @@
+`timescale 1ns / 1ps
+
+module data_packager(
+    input   logic           clk,
+    input   logic           arst_n,             // async active low reset
+    
+    // control from polling module
+    input   logic           poll_trigger,
+    
+    // inputs from sensors
+    input   logic [15:0]    i_gps,
+    input   logic [15:0]    i_accel,
+    input   logic [15:0]    i_gyro,
+    input   logic [7:0]     i_temp,
+    
+    // interface to BRAM buffer
+    output  logic [63:0]    o_packaged_word,    // packaged data
+    output  logic           o_we,               // write enable
+    output  logic [9:0]     o_addr              // address of BRAM
+    );
+    
+    logic [63:0]            current_frame;
+    logic [7:0]             rolling_ts;
+    
+    always_comb begin
+        current_frame = {
+            i_gps,          // [63:48]
+            i_accel,        // [47:32]
+            i_gyro,         // [31:16]
+            i_temp,         // [15:8]
+            rolling_ts      // [7:0]
+        };
+    end
+    
+    always_ff @(posedge clk or negedge arst_n) begin
+        if (!arst_n) begin
+            rolling_ts          <= '0;
+            o_addr              <= '0;
+            o_we                <= 1'b0;
+            o_packaged_word     <= '0;
+        end else begin
+            if (poll_trigger) begin
+                o_packaged_word <= current_frame;
+                o_we            <= 1'b1;
+                o_addr          <= o_addr + 1'b1;
+                rolling_ts      <= rolling_ts + 1'b1;
+            end else begin
+                o_we            <= 1'b0;
+            end
+        end
+    end
+    
+endmodule
