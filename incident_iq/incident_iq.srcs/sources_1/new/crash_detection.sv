b9a43ddb156eb0966cab2184f2351f6de6985e0c
RichardPC <richkejie@gmail.com>
2026-02-08 01:46:16 -0500

    add skeleton of crash detection and begin connecting in top

--- incident_iq/incident_iq.srcs/sources_1/new/crash_detection.sv
+++ incident_iq/incident_iq.srcs/sources_1/new/crash_detection.sv
@@ -1,6 +1,30 @@
 `timescale 1ns / 1ps
 
 module crash_detection(
+        input   logic           clk,
+        input   logic           rst,
 
+        input   logic [63:0]    i_packet,
+        input   logic           i_packet_valid,
+
+        input   logic [15:0]    i_accel_threshold,
+        input   logic [15:0]    i_orient_threshold,
+        input   logic [15:0]    i_temp_threshold,
+
+        output  logic           o_crash_detected
     );
+
+    // temporary...
+    assign o_crash_detected = 1'b0;
+
+    // store X past packets
+    // can be in a buffer/shift register
+
+    // calculate a moving average of the X history of packets
+
+    // if accel > accel_threshold, trigger crash detected
+    // if orient > orient_threshold, trigger crash detected
+    // if temp > temp_threshold, trigger crash detected
+
+
 endmodule
