4e55b1e7ae09fb72cff462bc38e83d123e1d7029
RichardPC <richkejie@gmail.com>
2026-02-23 10:55:40 -0500

    adding test project for sd card write: set up block design and initial sdk files

---
+++ sd-card-write-test/sd-card-write/sd-card-write.srcs/sources_1/bd/system_top/hdl/system_top_wrapper.v
@@ -0,0 +1,80 @@
+//Copyright 1986-2018 Xilinx, Inc. All Rights Reserved.
+//--------------------------------------------------------------------------------
+//Tool Version: Vivado v.2018.3 (win64) Build 2405991 Thu Dec  6 23:38:27 MST 2018
+//Date        : Mon Feb 23 00:05:28 2026
+//Host        : Richard_PC running 64-bit major release  (build 9200)
+//Command     : generate_target system_top_wrapper.bd
+//Design      : system_top_wrapper
+//Purpose     : IP block netlist
+//--------------------------------------------------------------------------------
+`timescale 1 ps / 1 ps
+
+module system_top_wrapper
+   (acl_spi_io0_io,
+    acl_spi_io1_io,
+    acl_spi_sck_io,
+    acl_spi_ss_io,
+    reset,
+    sys_clock);
+  inout acl_spi_io0_io;
+  inout acl_spi_io1_io;
+  inout acl_spi_sck_io;
+  inout acl_spi_ss_io;
+  input reset;
+  input sys_clock;
+
+  wire acl_spi_io0_i;
+  wire acl_spi_io0_io;
+  wire acl_spi_io0_o;
+  wire acl_spi_io0_t;
+  wire acl_spi_io1_i;
+  wire acl_spi_io1_io;
+  wire acl_spi_io1_o;
+  wire acl_spi_io1_t;
+  wire acl_spi_sck_i;
+  wire acl_spi_sck_io;
+  wire acl_spi_sck_o;
+  wire acl_spi_sck_t;
+  wire acl_spi_ss_i;
+  wire acl_spi_ss_io;
+  wire acl_spi_ss_o;
+  wire acl_spi_ss_t;
+  wire reset;
+  wire sys_clock;
+
+  IOBUF acl_spi_io0_iobuf
+       (.I(acl_spi_io0_o),
+        .IO(acl_spi_io0_io),
+        .O(acl_spi_io0_i),
+        .T(acl_spi_io0_t));
+  IOBUF acl_spi_io1_iobuf
+       (.I(acl_spi_io1_o),
+        .IO(acl_spi_io1_io),
+        .O(acl_spi_io1_i),
+        .T(acl_spi_io1_t));
+  IOBUF acl_spi_sck_iobuf
+       (.I(acl_spi_sck_o),
+        .IO(acl_spi_sck_io),
+        .O(acl_spi_sck_i),
+        .T(acl_spi_sck_t));
+  IOBUF acl_spi_ss_iobuf
+       (.I(acl_spi_ss_o),
+        .IO(acl_spi_ss_io),
+        .O(acl_spi_ss_i),
+        .T(acl_spi_ss_t));
+  system_top system_top_i
+       (.acl_spi_io0_i(acl_spi_io0_i),
+        .acl_spi_io0_o(acl_spi_io0_o),
+        .acl_spi_io0_t(acl_spi_io0_t),
+        .acl_spi_io1_i(acl_spi_io1_i),
+        .acl_spi_io1_o(acl_spi_io1_o),
+        .acl_spi_io1_t(acl_spi_io1_t),
+        .acl_spi_sck_i(acl_spi_sck_i),
+        .acl_spi_sck_o(acl_spi_sck_o),
+        .acl_spi_sck_t(acl_spi_sck_t),
+        .acl_spi_ss_i(acl_spi_ss_i),
+        .acl_spi_ss_o(acl_spi_ss_o),
+        .acl_spi_ss_t(acl_spi_ss_t),
+        .reset(reset),
+        .sys_clock(sys_clock));
+endmodule
---
+++ sd-card-write-test/sd-card-write/sd-card-write.srcs/sources_1/bd/system_top/ip/system_top_axi_quad_spi_0_0/system_top_axi_quad_spi_0_0_board.xdc
@@ -0,0 +1,6 @@
+#--------------------Physical Constraints-----------------
+
+set_property BOARD_PIN {acl_miso_i} [get_ports io0_t]
+set_property BOARD_PIN {acl_mosi_i} [get_ports io1_t]
+set_property BOARD_PIN {acl_ss_i} [get_ports ss_t]
+set_property BOARD_PIN {acl_sclk_i} [get_ports sck_t]
