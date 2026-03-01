523669173bac634dc2995b68b24efb04c6b6b79c
RichardPC <richkejie@gmail.com>
2026-03-01 13:13:41 -0500

    exclude microblaze stuff for mid-project-demo

--- incident_iq/incident_iq.srcs/sources_1/new/top.sv
+++ incident_iq/incident_iq.srcs/sources_1/new/top.sv
@@ -11,26 +11,28 @@ module top(
     logic [63:0] w_data_packet;
     logic w_data_packet_valid;
     
-    logic [31:0] w_data_packet_bram_addr;
-    logic [31:0] w_data_packet_bram_din;
-    logic [3:0] w_data_packet_bram_we;
-    logic w_data_packet_bram_en;
+//    logic [31:0] w_data_packet_bram_addr;
+//    logic [31:0] w_data_packet_bram_din;
+//    logic [3:0] w_data_packet_bram_we;
+//    logic w_data_packet_bram_en;
+    
+    // for mid-project-demo, don't worry about microblaze stuff
     
     // --- system top wrapper ---
-    system_top_wrapper u_system_top(
-        .diff_clock_rtl_clk_n(), // differential clock n --- determine where to connect on board
-        .diff_clock_rtl_clk_p(), // differential clock p
-        .aresetn(ARESET_N),
+//    system_top_wrapper u_system_top(
+//        .diff_clock_rtl_clk_n(), // differential clock n --- determine where to connect on board
+//        .diff_clock_rtl_clk_p(), // differential clock p
+//        .aresetn(ARESET_N),
         
-        // data_packet_mem BRAM_PORTB
-        .data_packet_bram_port_addr(w_data_packet_bram_addr),
-        .data_packet_bram_port_clk(CLK),
-        .data_packet_bram_port_din(w_data_packet_bram_din),
-        .data_packet_bram_port_dout(),      // unconnected, data packager does not need to read from the buffer
-        .data_packet_bram_port_en(w_data_packet_bram_en),
-        .data_packet_bram_port_rst(ARESET_N),  // resets dout, so not necessary since not reading
-        .data_packet_bram_port_we(w_data_packet_bram_we)         // 4-byte we   
-    );
+//        // data_packet_mem BRAM_PORTB
+//        .data_packet_bram_port_addr(w_data_packet_bram_addr),
+//        .data_packet_bram_port_clk(CLK),
+//        .data_packet_bram_port_din(w_data_packet_bram_din),
+//        .data_packet_bram_port_dout(),      // unconnected, data packager does not need to read from the buffer
+//        .data_packet_bram_port_en(w_data_packet_bram_en),
+//        .data_packet_bram_port_rst(ARESET_N),  // resets dout, so not necessary since not reading
+//        .data_packet_bram_port_we(w_data_packet_bram_we)         // 4-byte we   
+//    );
     
     // -- crash detection ---
     crash_detection u_crash_detection(
@@ -68,10 +70,10 @@ module top(
         .o_packet_valid     (w_data_packet_valid),
         
         // BRAM interface
-        .o_data_packet_bram_addr(w_data_packet_bram_addr),
-        .o_data_packet_bram_din(w_data_packet_bram_din),
-        .o_data_packet_bram_we(w_data_packet_bram_we),
-        .o_data_packet_bram_en(w_data_packet_bram_en),
+        .o_data_packet_bram_addr(),
+        .o_data_packet_bram_din(),
+        .o_data_packet_bram_we(),
+        .o_data_packet_bram_en(),
         
         // Crash Detection interface
         .o_cd_accel_max     (cd_accel_max),
